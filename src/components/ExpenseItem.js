b5a0ccfb078bcf2bd0ff549989cf844e74b12f00
Nikhil M <nikhil.mecheril@gmail.com>
2023-10-03 21:48:55 +0530

    V1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,19 @@
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css'
 
 
 function ExpenseItem(props){
+   
     return (
-        <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <div>{props.title}</div>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-        </div>
-    )
+        </Card>
+    );
 }
 
 export default ExpenseItem;
