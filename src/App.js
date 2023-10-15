f6d76548d827918e9a305d43ad60a43d759ee3f4
Nikhil M <nikhil.mecheril@gmail.com>
2023-10-15 23:31:47 +0530

    Updated components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
     {
-      id: 'e1',
-      title: 'Deluluu',
+      id: "e1",
+      title: "Deluluu",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'BMW', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "BMW", amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
-      title: 'Mercedes benz',
+      id: "e3",
+      title: "Mercedes benz",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'More delululuuu',
+      id: "e4",
+      title: "More delululuuu",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
   ];
 
+  const addExpenseHandler = (expense) => {};
+
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense = {addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
